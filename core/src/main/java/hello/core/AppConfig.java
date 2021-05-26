a49b5863f8593ae0d40e189a715e5c4076c88202
sangmokchae <72491390+SangMokChae@users.noreply.github.com>
2021-05-26 23:15:34 +0900

    ApplicationContext
    ApplicationContext .getBean
    start real spring

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 설정 정보 // import 주의
 public class AppConfig {
 
     // 여기서 생성자 주입 MemoryMemberRepository를 주입해준다.
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // ctrl + alt + m 중복되는 것들도 한번에 바꾸어 준다.
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
     // 나중에 바꿀때 위의 코드만 변경해주면 된다.
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,22 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp { // 실행되는지 테스트
 
     public static void main(String[] args) { // 단축키 psvm
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
         // memberService 안에 memberServiceImpl이 존재한다.
         // MemberService memberService = new MemberServiceImpl();
+
+        // SpringContainer 생성
+        // Annotaition 기반 설정
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP); // ctrl + alt + v
         memberService.join(member); // 회원가입
 
