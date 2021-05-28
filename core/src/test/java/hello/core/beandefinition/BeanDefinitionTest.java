d5accc7d3ef7652d5832115e612a288d9e39f77a
sangmokchae <72491390+SangMokChae@users.noreply.github.com>
2021-05-28 23:56:47 +0900

    bean definition test
    bean definition test

--- core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -1,4 +1,43 @@
 package hello.core.beandefinition;
 
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class BeanDefinitionTest {
+
+    // applicationContext로 하면 getBeanDefinition()을 사용하기 힘들다.
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext xac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        " beanDefinition = " + beanDefinition);
+            }
+        }
+    } // 강의 자료 참조
+
+    @Test // 위의 코드와 유사하지만.. 일단 쳐봄
+    @DisplayName("XML 설정 메타정보 확인")
+    void findXMLApplicationBean() { 
+        String[] beanDefinitionNames = xac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = xac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName + = " + beanDefinitionName +
+                        " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
 }
